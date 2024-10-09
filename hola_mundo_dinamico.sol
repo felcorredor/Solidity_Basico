1182467cb91df8ff1b10df3219d1bf0f0c6af733
felcorredor <felcorredor@unillanos.edu.co>
2024-10-08 19:41:32 -0500

    contrato dinamico de saludo

---
+++ hola_mundo_dinamico.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.24;
+
+contract hola_mundo_dinamico {
+    string Saludo_d = "Hola Grupo - dinamico";
+    string public Saluodo_e = "Saludo inicial en el despliegue";
+
+    function leerSaludo() public view returns (string memory ) {
+        return Saludo_d;
+    }
+
+    function guardarSaludo(string memory _nuevoSaludo) public {
+        Saludo_d = _nuevoSaludo;
+    }
+}
