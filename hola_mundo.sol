9741944dd649a6f33c2d3052e1463cf2d96d2519
felcorredor <felcorredor@unillanos.edu.co>
2024-10-08 19:16:32 -0500

    Primer contrato

---
+++ hola_mundo.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+//0x4c99ea78552112722879be3fd60d79d5cf521e2f
+pragma solidity 0.8.24;
+
+contract hola_mundo{
+
+    string  public saludo = "Hola grupo . Att. yo";
+
+}
